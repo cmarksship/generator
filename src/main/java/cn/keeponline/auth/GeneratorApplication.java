554fbdc0c78450a9bc1895217a5c375a6f120b07
LAPTOP-L3PTTHJJ\marks <marksship@outlook.com>
2023-07-04 12:01:01 +0400

    去掉多余引用

--- src/main/java/cn/keeponline/auth/GeneratorApplication.java
+++ src/main/java/cn/keeponline/auth/GeneratorApplication.java
@@ -1,18 +1,14 @@
 package cn.keeponline.auth;
 
-import cn.keeponline.common.swagger.annotation.EnableCustomSwagger2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import cn.keeponline.common.security.annotation.EnableRyFeignClients;
 
 /**
  * 认证授权中心
  * 
  * @author capital
  */
-@EnableCustomSwagger2
-@EnableRyFeignClients
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GeneratorApplication {
     public static void main(String[] args) {
